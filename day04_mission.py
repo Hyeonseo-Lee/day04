3ba66e62593cd9e1e10f1762091604f8e5643d64
Hyeonseo-Lee <lhss1054@naver.com>
2023-01-17 18:52:20 +0900

    8_1,2,3,4,5

--- day04_mission.py
+++ day04_mission.py
@@ -1,38 +1,8 @@
-#7.11
-
-start1 = ["fee", "fie", "foe"]
-rhymes = [
- ("flop", "get a mop"),
- ("fope", "turn the rope"),
- ("fa", "get your ma"),
- ("fudge", "call the judge"),
- ("fat", "pet the cat"),
- ("fog", "walk the dog"),
- ("fun", "say we're done"),
- ]
-
-start2 = "Someone better"
-
-
-start1 = '! '.join(start1)
-start1 = start1 + '!'
-start2 = start2 + ' '
-for number in range(7):
-    rhymes1 = rhymes[number][0]
-    answer1 = start1 + ' ' + rhymes1 + '!'
-    answer1 = answer1.title()
-    #print(rhymes1)
-    #print(start1)
-    print(answer1)
-    new_rhymes = rhymes[number][1]
-    new_rhymes = start2 + new_rhymes
-    print(new_rhymes)
-
-
-
-
-
-
-
-
+#8.1,2,3,4,5
+e2f = {'dog': 'chien', 'cat': 'chat', 'walrus': 'morse'}
+print(e2f['walrus'])
+f2e = {v:k for k,v in e2f.items()}
+print(f2e)
+print(f2e['chien'])
+print(f2e.keys())
 

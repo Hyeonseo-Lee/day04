e4c07eef8349bcc2207b49b07b269185c95aea0b
Hyeonseo-Lee <lhss1054@naver.com>
2023-01-17 18:08:49 +0900

    7_11

--- day04_mission.py
+++ day04_mission.py
@@ -1,5 +1,38 @@
-#7.10
+#7.11
 
-a_list = [number for number in range(10) if number % 2 == 0]
-del a_list[0]
-print(a_list)
+start1 = ["fee", "fie", "foe"]
+rhymes = [
+ ("flop", "get a mop"),
+ ("fope", "turn the rope"),
+ ("fa", "get your ma"),
+ ("fudge", "call the judge"),
+ ("fat", "pet the cat"),
+ ("fog", "walk the dog"),
+ ("fun", "say we're done"),
+ ]
+
+start2 = "Someone better"
+
+
+start1 = '! '.join(start1)
+start1 = start1 + '!'
+start2 = start2 + ' '
+for number in range(7):
+    rhymes1 = rhymes[number][0]
+    answer1 = start1 + ' ' + rhymes1 + '!'
+    answer1 = answer1.title()
+    #print(rhymes1)
+    #print(start1)
+    print(answer1)
+    new_rhymes = rhymes[number][1]
+    new_rhymes = start2 + new_rhymes
+    print(new_rhymes)
+
+
+
+
+
+
+
+
+

b3344d0b97b84d53063e10c03d7c00a8f3cea96c
Hyeonseo-Lee <lhss1054@naver.com>
2023-01-17 17:03:53 +0900

    7_9

--- day04_mission.py
+++ day04_mission.py
@@ -1,6 +1,13 @@
-#7.8
+#7.9
 surprise = ["Groucho", "Chico", "Harpo"]
 surprise[2] = surprise[2].lower()
+surprise_string = ''.join(surprise[2])
+surprise_string = surprise_string[::-1]
+surprise_string = surprise_string.capitalize()
+surprise[2] = surprise_string
+print(surprise)
+
+
 
 
 

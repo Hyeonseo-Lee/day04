adf54ee01285f6c94d411bc28c7c9529d6f12a08
Hyeonseo-Lee <lhss1054@naver.com>
2023-01-17 19:14:02 +0900

    8_6,7,8,9

--- day04_mission.py
+++ day04_mission.py
@@ -1,8 +1,6 @@
-#8.1,2,3,4,5
-e2f = {'dog': 'chien', 'cat': 'chat', 'walrus': 'morse'}
-print(e2f['walrus'])
-f2e = {v:k for k,v in e2f.items()}
-print(f2e)
-print(f2e['chien'])
-print(f2e.keys())
+#8.6,7,8,9
+dict={'animals':{'cats':'Henri', 'octopi':'Grumpy', 'emus': 'Lucy'}, 'plants':{}, 'other': {}}
+print(dict.keys())
+print(dict['animals'].keys())
+print(dict['animals']['cats'])
 

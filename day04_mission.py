094046d0fe0739cbfc0aa6fb9cc5a3412dd5770f
Hyeonseo-Lee <lhss1054@naver.com>
2023-01-17 19:42:15 +0900

    8_13,14

--- day04_mission.py
+++ day04_mission.py
@@ -1,9 +1,11 @@
-#8.10
-squares = {key: key**2 for key in range(10)}
-print(squares)
+#8.13
+key = ('optimist', 'pessimist', 'troll')
+value = ('The glass is half full', 'The glass is half empty', 'How did you get a glass?')
+diction = dict(zip(key,value))
+print(diction)
 
-# for key in range(10):
-#     squares={key:key**2}
-#     print(squares)
-
-
+#8.14
+titles = ['Creature of Habit', 'Crewel Fate', 'Sharks On a Plane']
+plots = ['A nun turns into a monster', 'A haunted yarn shop', 'Check your exits']
+movies = dict(zip(titles, plots))
+print(movies)

27b47e70facb13e5ac6f256f84f3cf3d409e0541
Hyeonseo-Lee <lhss1054@naver.com>
2023-01-17 19:27:48 +0900

    8_10

--- day04_mission.py
+++ day04_mission.py
@@ -1,6 +1,9 @@
-#8.6,7,8,9
-dict={'animals':{'cats':'Henri', 'octopi':'Grumpy', 'emus': 'Lucy'}, 'plants':{}, 'other': {}}
-print(dict.keys())
-print(dict['animals'].keys())
-print(dict['animals']['cats'])
+#8.10
+squares = {key: key**2 for key in range(10)}
+print(squares)
+
+# for key in range(10):
+#     squares={key:key**2}
+#     print(squares)
+
 

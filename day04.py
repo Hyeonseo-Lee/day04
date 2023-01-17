0ad7b49ad54a67128a803b0fccbc378baef92c90
Hyeonseo-Lee <lhss1054@naver.com>
2023-01-17 11:18:37 +0900

    day04_2

--- day04.py
+++ day04.py
@@ -1,22 +1,33 @@
-#tuple
-a = 'harry',
-b = ('harry',)
-c=()
-d = 'harry', 'ron' #packing
-e = ('hermione') #string
-f = ('harry', 'ron') #packing
-g = ('hermione',)
-print(g, id(g))
-g = g+f
-print(g, id(g))
-print(g)
-print(g+f)
-print(f[1])
-x, y = f #unpacking
-print(y)
-print(type(a))
-print(type(b))
-print(type(c))
-print(type(d))
-print(type(e))
-
+#list
+big_bang = ['지성', '태양', '탑', '대성', '승리']
+exo = ['백현', '첸']
+#big_bang.append('인하')
+big_bang.insert(1, '인하')
+#print(big_bang*2)
+#exo.extend(big_bang)
+#exo = exo + big_bang
+exo.append(big_bang)
+print(exo)
+print(exo[2][2]) #태양
+print(exo[-1][-4]) #태양
+exo[-2] = '시우민'
+print(exo)
+#print(exo.pop()) #빅뱅 pop
+print((exo[2].pop())) #승리 pop
+print(exo)
+print((exo[2].pop(-2))) #탑 pop
+print(exo)
+del exo[2][-1] #대성 del
+print(exo)
+exo.remove('인하') #not in exo
+exo[2].remove('인하')
+print(exo)
+exo.clear()
+print(exo)
+#scores = ('B+', 'A+', 'C+')
+#print(scores[1], scores[2])
+# temp = list(scores)
+# temp[1] = 'C+'
+# temp[2] = 'A+'
+# scores = tuple(temp)
+# print(scores[1], scores[2])
